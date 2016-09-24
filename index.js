d6b8b40b01b666a689f9b3eb1cab35eb03fd3411
Roberto Mariotti <github@robertomariotti.com>
2016-09-24 07:38:05 +0200

    R0

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var express = require('express');
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+
+
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -16,5 +27,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
