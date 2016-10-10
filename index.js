af8e93e77e147880533de4f2ca7f7256383edd26
Roberto Mariotti <github@robertomariotti.com>
2016-10-10 12:49:43 +0200

    R3-fullbase

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
 var express = require('express');
-var hbs = require('express-hbs');
+var hbs = require('hbs');
 var path = require('path');
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var loki = require('lokijs');
+var session      = require('express-session');
+
+var LocalStrategy   = require('passport-local').Strategy;
+var passport = require('passport');
 
 
 var db = new loki('bolloni.json')
-var bolloni = db.addCollection('bolloni')
-
+var bolloni = db.addCollection('bolloni', { unique: ['id'] });
+var bolloniHistory = db.addCollection('history'); //, { unique: ['id'] });
+var conf = db.addCollection('conf', { unique: ['userId'] });
 
 var parts = require('./routes/service');
 
-var bolloniConf = {
-  rows: [
-    {bolloni: [
-      {id : 'bollone1', style : 'col-xs-12 col-sm-6'},
-      {id : 'bollone2', style : 'col-xs-12 col-sm-6'}
-    ]},
-    {bolloni: [
-      {id : 'bollone3', style : 'col-xs-6 col-sm-3'},
-      {id : 'bollone4', style : 'col-xs-6 col-sm-3'},
-      {id : 'bollone5', style : 'col-xs-6 col-sm-3'},
-      {id : 'bollone6', style : 'col-xs-6 col-sm-3'}
-    ]}
-  ]
-}
 
+bolloni.insert([
+  {id : 'bollone1', style : 'b1', category : { desc : 'Categoria uno', style : ''}, desc : 'Bollone uno'    , img : 'b1.jpeg'},
+  {id : 'bollone2', style : 'b1', category : { desc : 'Categoria uno', style : ''}, desc : 'Bollone due'    , img : 'b2.jpeg'},
+  {id : 'bollone3', style : 'b1', category : { desc : 'Categoria uno', style : ''}, desc : 'Bollone tre'    , img : 'b3.jpg' },
+  {id : 'bollone4', style : 'b1', category : { desc : 'Categoria due', style : ''}, desc : 'Bollone quattro', img : 'b4.jpeg'},
+  {id : 'bollone5', style : 'b1', category : { desc : 'Categoria due', style : ''}, desc : 'Bollone cinque' , img : 'b5.jpg' },
+  {id : 'bollone6', style : 'b1', category : { desc : 'Categoria due', style : ''}, desc : 'Bollone sei '   , img : 'b5.jpg' }
+]);
+
+var userConf = {
+                userId  : '1',
+                style   : '3',
+                rows    : [{ id: 'row1',
+                             bolloni: [
+                              { id : 'bollone1', style : 'col-xs-4 col-sm-3'},
+                              { id : 'bollone2', style : 'col-xs-4 col-sm-3'},
+                              { id : 'bollone3', style : 'col-xs-4 col-sm-3'},
+                              { id : 'bollone3', style : 'col-xs-4 col-sm-3'},
+                              { id : 'bollone4', style : 'col-xs-4 col-sm-3'},
+                              { id : 'bollone5', style : 'col-xs-4 col-sm-3'},
+                              { id : 'bollone6', style : 'col-xs-4 col-sm-3'}
+                            ]},
+                            { id: 'row2',
+                              bolloni: [
+                                { id : 'bollone4', style : 'col-xs-4 col-sm-3'},
+                                { id : 'bollone5', style : 'col-xs-4 col-sm-3'},
+                                { id : 'bollone6', style : 'col-xs-4 col-sm-3'}
+                            ]},
+                            { id: 'row3',
+                              bolloni: [
+                                { id : 'bollone4', style : 'col-xs-12 col-sm-3'},
+                                { id : 'bollone5', style : 'col-xs-12 col-sm-3'},
+                                { id : 'bollone6', style : 'col-xs-12 col-sm-3'}
+                            ]}
+
+                          ]
+              };
+
+conf.insert(userConf);
 
 var app = express();
 
 
-app.use('/se', parts);
+//login
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use('/se', parts);
 app.use('/pu', express.static(path.join(__dirname, 'public')));
 app.use('/co', express.static(path.join(__dirname, 'bower_components')));
+app.use('/pa', express.static(path.join(__dirname, 'views/partials')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -45,38 +81,110 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 
 
 // views is directory for all template files
-app.engine('hbs', hbs.express4({
-  partialsDir: __dirname + '/views'
-}));
+//app.engine('hbs', hbs.express4({
+//  partialsDir: __dirname + '/views'
+//}));
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
+hbs.registerPartials(__dirname + '/views/partials');
 
+hbs.registerHelper('json', (context) => JSON.stringify(context));
+
+hbs.registerHelper('bolloneEnrich', function(configBollone, bolloniList, history, options) {
+    return options.fn({conf : this,
+                       bollone : bolloniList.filter((bollone) => bollone.id===configBollone.id)[0],
+                       history : history.filter((bollone) => bollone.id===configBollone.id)
+                     })
+   }
+);
+
+hbs.registerHelper('currentBolloneEnrich', function(bolloniList, history, options) {
+    return history[0] ? options.fn({
+                       bollone : bolloniList.filter((bollone) => history[0].id === bollone.id)[0],
+                       currentBollone : history[0],
+                       calculatedDuration : Date.now() - history[0].start
+                     }) :'';
+   }
+);
+
+
+app.set('conf', conf);
 app.set('bolloni', bolloni);
+app.set('bolloniHistory', bolloniHistory);
 
-app.get('/', function(request, response) {
-  response.render('index', {title: 'Bolloni',
-                            bolloni : {
-                              rows: [
-                                {bolloni: [
-                                  {id : 'bollone3', img: 'b4.jpeg' , style : 'col-xs-6 col-sm-3'},
-                                  {id : 'bollone4', img: 'b5.jpg' , style : 'col-xs-6 col-sm-3'},
-                                  {id : 'bollone5', img: 'b6.jpg' , style : 'col-xs-6 col-sm-3'},
-                                  {id : 'bollone6', img: 'b1.jpeg' , style : 'col-xs-6 col-sm-3'}
-                                ]},
-                                {bolloni: [
-                                  {id : 'bollone7', img: 'b1.jpeg' , style : 'col-xs-4 col-sm-2'},
-                                  {id : 'bollone8', img: 'b2.jpeg' , style : 'col-xs-4 col-sm-2'},
-                                  {id : 'bollone9', img: 'b3.jpg' , style : 'col-xs-4 col-sm-2'},
-                                  {id : 'bollone0', img: 'b1.jpeg' , style : 'col-xs-4 col-sm-2'},
-                                  {id : 'bollone1', img: 'b6.jpg' , style : 'col-xs-4 col-sm-2'},
-                                  {id : 'bollone2', img: 'b1.jpeg' , style : 'col-xs-4 col-sm-2'}
-                                ]},
-                              ]
-                            },
-                            layout: 'layout/default'});
+
+function formatPageData(formatStyle, bolloniList, currentIndex, result){
+  //col-xs-12 col-sm-6
+
+  if(currentIndex<bolloniList.length)
+    return formatPageData(formatStyle, bolloniList, currentIndex++, result)
+  else
+    return result;
+}
+
+function loadUserConfiguration(userId){
+  return conf.by('userId', userId);
+}
+
+function getNeededBolloni(configuration){
+  return configuration.rows.reduce((p, c) =>
+     p.concat(c.bolloni.reduce((p, c) =>
+       p.concat([c.id])
+      ,[]))
+  , [])
+  .reduce((p, c, i, a) =>
+    p.indexOf(c)<0 ? p.concat([c]) : p
+  , []).map((item) => {return  bolloni.by('id',item)});
+}
+
+function getHistoryBolloni(history){
+  return history.chain().simplesort('start', true).data();
+}
+
+app.get('/', isLoggedIn, function(request, response) {
+
+  console.log(request.user);
+
+  var userId = 1;
+
+  var conf = loadUserConfiguration(userId);
+
+  var neededBolloni = getNeededBolloni(conf);
+
+  var history = getHistoryBolloni(bolloniHistory);
+
+  var pageData = {title: 'Bolloni',
+                            user : request.user,
+                            configuration : conf,
+                            bolloni : neededBolloni,
+                            history : history,
+                            layout: 'layout/default'};
+
+  response.render('index', pageData);
 });
+
+
+app.get('/login', function(req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('login', {bolloni : {}, title: 'Bolloni login', layout: 'layout/default'});
+});
+
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+}));
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
@@ -88,4 +196,33 @@ process.on('SIGTERM', function () {
     db.saveDatabase();
     process.exit(0);
   });
+});
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/login');
+}
+
+
+passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'inputEmail',
+        passwordField : 'inputPassword',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, user, password, done) {
+          done(null, { "user": user});
+    })
+);
+
+
+passport.serializeUser(function(user, done) {
+    done(null, "a");
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+     done(null, { "user": id});
 });
--- routes/service.js
+++ routes/service.js
@@ -1,48 +1,86 @@
 var Loki = require ('lokijs');
 var express = require('express');
+var app = require('../index.js');
 var router = express.Router();
 
 
 
 
+function getHistory(bolloniHistory){
+  return bolloniHistory ? bolloniHistory.chain().simplesort('start', true).data() : {}
+}
+
+
 /* GET users listing. */
-router.get('/:bollone', function(req, res){
+router.get('/bolloni', function(req, res){
+  var bolloniHistory = req.app.get('bolloniHistory');
   var bolloni = req.app.get('bolloni');
-  var bolloneId = req.params.bollone;
+  var conf = req.app.get('conf');
 
-  var results = bolloni.where(function(obj) {
-    console.log(JSON.stringify(obj));
-    return obj.id === bolloneId;
-  });
-
-  console.log('Get ' + req.params.bollone);
-  res.json(results);
+  res.json(bolloni.data());
 });
 
+router.get('/history', function(req, res){
+  var bolloniHistory = req.app.get('bolloniHistory');
+  var bolloni = req.app.get('bolloni');
+  var conf = req.app.get('conf');
+
+  res.json(getHistory(bolloniHistory));
+});
+
+router.get('/:bollone', function(req, res){
+  var bolloniHistory = req.app.get('bolloniHistory');
+  var bolloni = req.app.get('bolloni');
+  var conf = req.app.get('conf');
+
+  var bolloneId = req.params.bollone;
+  var bollone = bolloni.by('id', bolloneId);
+
+  //console.log('Get ' + req.params.bollone);
+  res.json(getHistory(bolloniHistory));
+});
+
+function formatResponse(currentBollone, changedBolloni, listBolloni){
+  return {
+      history : getHistory()
+    };
+}
+
+function closeBollone(item) {
+  var newItem = item;
+  newItem.end = Date.now();
+  newItem.duration = newItem.end - newItem.start;
+  return newItem;
+}
+
+function newBollone(bolloneId) {
+  return {id: bolloneId, start: Date.now()};
+}
 
 router.post('/:bollone', function(req, res){
+  var bolloniHistory = req.app.get('bolloniHistory');
   var bolloni = req.app.get('bolloni');
+  var conf = req.app.get('conf');
+
   var bolloneId = req.params.bollone;
 
-  var results = bolloni.where(function(obj) {
-    console.log(JSON.stringify(obj));
-    return obj.id === bolloneId;
-  });
+  console.log("Bollone " + bolloneId + app + app.bolloni + app.bolloniHistory + app.conf);
 
-  var obj;
-  if(results.length>0){
-    obj = results[0];
-    obj.end = Date.now();
-    obj.duration = obj.end - obj.start;
-    bolloni.update(obj)
-  }
-  else {
-    obj = {id: bolloneId, start: Date.now()};
-    bolloni.insert(obj);
-  }
-  console.log('Post ' + req.params.bollone + " = " + JSON.stringify(obj));
+  var isOpen = bolloniHistory.where(function(obj) {return obj.id === bolloneId && !obj.end});
 
-  res.json(obj);
+  bolloniHistory.where(function(obj) {return !obj.end})
+    .map((item) => {
+      console.log("Closing " + item.id);
+      return bolloniHistory.update(closeBollone(item));
+    });
+
+    if(isOpen.length==0) {
+      console.log("Bollone not exists or is closed " + bolloneId);
+      bolloniHistory.insertOne(newBollone(bolloneId));
+    }
+
+
+  res.json(getHistory(bolloniHistory));
 });
 
 module.exports = router;
